4fe59d3f6d3d91656c72bfafbc5d249caaa091de
James Cat <james@catplusplus.org.uk>
2019-07-18 09:17:57 +0100

    test file

--- src/test/tests.js
+++ src/test/tests.js
@@ -1,113 +1,39 @@
-// import {counter} from '../js/reducer'
-// import {addCounter, removeCounter, incrementCounter, toggleTodo} from  '../js/addCounter'
-// import {todoApp} from '../js/todos'
-
-// const expect = require("expect");
+import {cellar} from '../js/reducer-bottles'
+import {addBottle,setVisibilityFilter,setUiMode, showDetail} from  '../js/action-creators'
+import * as C from './constants'
 
 
-// console.log('tests running');
-
-// expect(
-//   counter(0, {type: 'INCREMENT'})
-// ).toEqual(1);
-
-// expect(
-//   counter(1, {type: 'INCREMENT'})
-// ).toEqual(2);
-
-// expect(
-//   counter(2, {type: 'DECREMENT'})
-// ).toEqual(1);
-
-// expect(
-//   counter(1, {type:'SOMETHING ELSE'})
-// ).toEqual(1);
-
-// expect(
-//   counter(undefined, {})
-// ).toEqual(0);
-
-// expect(addCounter([])).toEqual([0]);
-
-// expect(
-//   removeCounter([0,10,20],1)
-// ).toEqual([0,20])
-
-// const listBefore = [0,10,20];
-// const listAfter = [0,11,20];
-
-// expect(
-//   incrementCounter(listBefore,1)
-// ).toEqual(listAfter);
-
-// const todoBefore = {
-//   id:0,
-//   text:'Learn Redux',
-//   completed: false
-// };
-// const todoAfter = {
-//   id:0,
-//   text:'Learn Redux',
-//   completed: true
-// };
-
-// expect(
-//   toggleTodo(todoBefore)
-// ).toEqual(todoAfter);
+const expect = require("expect");
 
 
+console.log('tests running');
 
-// var stateBefore = []
-// var action = {
-//   type: 'ADD_TODO',
-//   id: 0,
-//   text: 'Learn Redux'
-// };
-// var stateAfter = [
-//   {
-//     id: 0,
-//     text: 'Learn Redux',
-//     completed: false
-//   }
-// ]
+// actions to text
+// addBottle(name, colour, detail)
+// setVisibilityFilter(filter)
+// setUiMode(mode, id)
+// showDetail(id)
 
-// expect(
-//   todos(stateBefore, action)
-// ).toEqual(stateAfter)
 
-// var stateBefore = [
-//   {
-//     id: 0,
-//     text: 'Learn Redux',
-//     completed: false
-//   },
-//   {
-//     id: 1,
-//     text: 'Go Shopping',
-//     completed: false
-//   }
-// ];
+// state shape
 
-// var action = {
-//   type: 'TOGGLE_TODO',
-//   id: 1
-// }
+// bottles : array [{        
+//   id     : int,
+//   name   : string,
+//   colour : string,
+//   detail : string
+// }]
+// visibilityFilter : string
+// uimode : string 
+// detail : string
 
-// var stateAfter = [
-//   {
-//     id: 0,
-//     text: 'Learn Redux',
-//     completed: false
-//   },
-//   {
-//     id: 1,
-//     text: 'Go Shopping',
-//     completed: true
-//   }
-// ];
+var stateBefore = {bottles: [], visibilityFilter: "SHOW_ALL", uimode: "LIST", detail: null}
+var action = addBottle(0, 'vin rouge', C.RED, 'lovely wine')
+var stateAfter = {bottles: [{id:0, name: "Cabernet Shiraz Merlot", colour: "RED", detail: 'lovely wine'}], visibilityFilter: "SHOW_ALL", uimode: "LIST", detail: null}
 
-// expect(
-//   todos(stateBefore, action)
-// ).toEqual(stateAfter)
+expect(
+  cellar(stateBefore, action)
+).toEqual(stateAfter)
 
-// console.log('tests passed');
+
+console.log('tests passed');
