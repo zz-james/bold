d4c34569b7b1e8403a2aed91672f0eac7fab323e
James Cat <james@catplusplus.org.uk>
2019-07-18 08:28:42 +0100

    some dummy wine data added

--- src/js/data.js
+++ src/js/data.js
@@ -7,21 +7,21 @@ export let data = {
   bottles : [
     {
       id     : uuidv4(),
-      name   : 'vin rouge',
+      name   : 'Cabernet Shiraz Merlot',
       colour : C.RED,
-      detail : 'lovely vin rouge yum yum'
+      detail : 'Bright Red Bouquet: Red fruits with light savoury notes, nutmeg & chocolate oak character Palate: Sweet ripe plum & berry fruits with a subtle vanilla & choc mint oak character. The wine is medium to full body with soft balanced tannins.'
     },
     {
       id     : uuidv4(),
-      name   :'vin blanc',
+      name   :'DINO Trebbiano Pinot Grigio',
       colour : C.WHITE,
-      detail : 'keep it in the fridge for hot days'
+      detail : 'Crisp and fruity, this wine created from Trebbiano and Pinot Grigio grapes grown in central Italy giving it fresh flavours of citrus and red apple. Serve chilled as an aperitif of with lightly spiced food. Store in a cool, dark place.'
     },
     {
       id     : uuidv4(),
-      name   : 'vin rose',
+      name   : 'Gallo Family Vineyards Rose Grenache',
       colour : C.OTHER,
-      detail : 'party wine'
+      detail : "You can taste the Gallo family's dedication to the art of winemaking, passed down through four generations, in this White Grenache. This delicious rosé highlights vibrant fresh strawberry flavours and delicate floral notes."
     },  
   ]
 }
