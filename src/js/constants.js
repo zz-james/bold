f5ebf4a0753a944daac32aba2b6370448c26ec2a
James Cat <james@catplusplus.org.uk>
2019-07-18 01:33:24 +0100

    constants

---
+++ src/js/constants.js
@@ -0,0 +1,18 @@
+export const ADD_BOTTLE = 'ADD_BOTTLE';
+
+export const SET_VISIBILITY_FILTER = 'SET_VISIBILITY_FILTER';
+export const SHOW_ALL = 'SHOW_ALL';
+export const SHOW_RED = 'SHOW_RED';
+export const SHOW_WHITE = 'SHOW_WHITE';
+export const SHOW_OTHER = 'SHOW_OTHER';
+
+export const SET_UI_MODE = 'SET_UI_MODE';
+export const LIST = 'LIST';
+export const EDIT = 'EDIT';
+export const DETAIL = 'DETAIL';
+
+export const SHOW_DETAIL = 'SHOW_DETAIL';
+
+export const RED = 'RED';
+export const WHITE = 'WHITE';
+export const OTHER = 'OTHER';
